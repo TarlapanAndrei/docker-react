59571270fd764874a0b82f70864923b29cab7849
Andrei Tarlapan <tarlapan@gmail.com>
2021-01-21 19:10:54 +0200

    check the feature branch

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> saver to reload master.
         </p>
+        <p> O Schimbare </p>
         <a
           className="App-link"
           href="https://reactjs.org"
